36c1769a193479176257141ef9f12209fc537cc4
hyunjuuun <hyunjun86120@gmail.com>
2024-03-05 08:44:30 +0900

    초기 세팅

---
+++ src/main/kotlin/com/hyun/moondab/MoondabApplication.kt
@@ -0,0 +1,11 @@
+package com.hyun.moondab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoondabApplication
+
+fun main(args: Array<String>) {
+	runApplication<MoondabApplication>(*args)
+}
