9cf6d27566fdd6add2beacb18f60e25549706113
hyunjuuun <hyunjun86120@gmail.com>
2024-03-14 22:29:20 +0900

    #10 common: frontend 추가

---
+++ frontend/lib/quiz.dart
@@ -0,0 +1,44 @@
+import 'package:adv_basics/question_screen.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color.fromARGB(255, 98, 51, 180),
+                  Color.fromARGB(255, 83, 29, 176),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: activeScreen == 'start-screen'
+                ? StartScreen(switchScreen)
+                : const QuestionScreen()),
+      ),
+    );
+  }
+}
