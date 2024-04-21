ae599d8b1188ef00b9004649458ecb2b2a797797
hyunjuuun <hyunjun86120@gmail.com>
2024-04-21 13:53:43 +0900

    Question class 식별자 추가

--- frontend/lib/models/questions.dart
+++ frontend/lib/models/questions.dart
@@ -1,6 +1,7 @@
 class Question {
-  const Question(this.text, this.month, this.day);
+  const Question(this.id, this.text, this.month, this.day);
 
+  final int id;
   final String text;
   final int month;
   final int day;
