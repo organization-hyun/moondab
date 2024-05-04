736ecf91cfcde11a3c9db3d4bf22af374958c0ad
hyunjuuun <hyunjun86120@gmail.com>
2024-05-04 10:18:08 +0900

    [#34] 서버 실행 시 기본 그룹 추가

---
+++ backend/src/main/java/com/hyun/moondab/config/init/InitData.java
@@ -0,0 +1,22 @@
+package com.hyun.moondab.config.init;
+
+import com.hyun.moondab.domain.group.Group;
+import com.hyun.moondab.domain.group.GroupRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class InitData implements ApplicationRunner {
+
+    private final GroupRepository groupRepository;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        Group group = Group.create("기본 그룹");
+        groupRepository.save(group);
+    }
+
+}
