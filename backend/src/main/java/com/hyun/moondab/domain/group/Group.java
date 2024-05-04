3122b34ca157784efa9dc59fe973cbc4d57ad437
hyunjuuun <hyunjun86120@gmail.com>
2024-05-04 10:17:47 +0900

    [#33] Group, GroupPost Entity 추가

--- backend/src/main/java/com/hyun/moondab/domain/group/Group.java
+++ backend/src/main/java/com/hyun/moondab/domain/group/Group.java
@@ -2,7 +2,9 @@ package com.hyun.moondab.domain.group;
 
 import com.hyun.moondab.domain.group.post.GroupPost;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @Getter
 @Entity
 @Table(name = "groups")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Group {
 
     @Id
@@ -18,7 +21,20 @@ public class Group {
 
     private String title;
 
-    @OneToMany(mappedBy = "group")
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<GroupPost> groupPosts = new ArrayList<>();
+
+    public static Group create(String title) {
+        return new Group(title);
+    }
+
+    private Group(String title) {
+        this.title = title;
+    }
+
+    public void addGroupPost(GroupPost groupPost) {
+        this.groupPosts.add(groupPost);
+        groupPost.setGroup(this);
+    }
 
 }
--- backend/src/main/java/com/hyun/moondab/domain/group/post/GroupPost.java
+++ backend/src/main/java/com/hyun/moondab/domain/group/post/GroupPost.java
@@ -2,8 +2,11 @@ package com.hyun.moondab.domain.group.post;
 
 import com.hyun.moondab.domain.group.Group;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GroupPost {
 
     @Id
@@ -15,5 +18,17 @@ public class GroupPost {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "group_id")
     private Group group;
+
+    public static GroupPost create(String content) {
+        return new GroupPost(content);
+    }
+
+    private GroupPost(String content) {
+        this.content = content;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
 
 }
