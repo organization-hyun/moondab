d637390ddc1aa8eebb8897d5a9b4e2b1b6b4c69a
hyunjuuun <hyunjun86120@gmail.com>
2024-05-13 23:03:58 +0900

    [#39] Group > 최근 일자 필드 추가

--- frontend/lib/models/group.dart
+++ frontend/lib/models/group.dart
@@ -1,13 +1,14 @@
 class Group {
-  Group({
-    required this.id,
-    required this.title,
-    required this.numOfPosts,
-    required this.numOfUsers,
-  });
+  Group(
+      {required this.id,
+      required this.title,
+      required this.numOfPosts,
+      required this.numOfUsers,
+      required this.latestDateTime});
 
   final int id;
   final String title;
   final int numOfPosts;
   final int numOfUsers;
+  final DateTime latestDateTime;
 }
