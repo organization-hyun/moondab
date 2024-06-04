50b15ac971aa864e6c05ec8c7d94efc860d52212
Hg-lab <siregy1222@gmail.com>
2024-06-04 23:34:32 +0900

    GroupPost, Group 을 초기화한다

--- backend/src/main/java/com/hyun/moondab/config/init/InitData.java
+++ backend/src/main/java/com/hyun/moondab/config/init/InitData.java
@@ -2,6 +2,8 @@ package com.hyun.moondab.config.init;
 
 import com.hyun.moondab.domain.group.Group;
 import com.hyun.moondab.domain.group.GroupRepository;
+import com.hyun.moondab.domain.group.post.GroupPost;
+import com.hyun.moondab.domain.group.post.GroupPostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -12,12 +14,25 @@ import org.springframework.stereotype.Component;
 public class InitData implements ApplicationRunner {
 
     private final GroupRepository groupRepository;
+    private final GroupPostRepository groupPostRepository;
 
     @Override
     public void run(ApplicationArguments args) {
+        Group group = initGroup();
+        initGroupPostByGroup(group);
+    }
+
+    private Group initGroup() {
         Group group = Group.create("기본 그룹");
         group.addGroupUser(1L);
-        groupRepository.save(group);
+        return groupRepository.save(group);
     }
+
+    private void initGroupPostByGroup(Group group) {
+        GroupPost groupPost = GroupPost.create("기본 게시글");
+        groupPost.setGroup(group);
+        groupPostRepository.save(groupPost);
+    }
+
 
 }
