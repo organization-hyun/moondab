d053de1efb1c5f8954ff78b0c4300b9f90d208bb
hyunjuuun <hyunjun86120@gmail.com>
2024-07-27 16:07:53 +0900

    Question create 경계값 테스트 추가

--- src/test/java/com/hyun/moondab/domain/question/QuestionTest.java
+++ src/test/java/com/hyun/moondab/domain/question/QuestionTest.java
@@ -1,10 +1,12 @@
 package com.hyun.moondab.domain.question;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 
 class QuestionTest {
 
@@ -12,17 +14,23 @@ class QuestionTest {
     @Test
     void create() {
         // given
-        int month = 1;
-        int day = 1;
         String content = "올해 꼭 이루고 싶은 목표가 있나요?";
+        int month1 = 1;
+        int day1 = 1;
+        int month12 = 12;
+        int day31 = 31;
 
         // when
-        Question question = Question.create(month, day, content);
+        Question question1 = Question.create(month1, day1, content);
+        Question question2 = Question.create(month12, day31, content);
 
         //then
-        assertThat(question)
+        assertThat(List.of(question1, question2))
                 .extracting("questionMonth", "questionDay", "content")
-                .containsExactly(month, day, content);
+                .containsExactly(
+                        tuple(month1, day1, content),
+                        tuple(month12, day31, content)
+                );
     }
 
 }
