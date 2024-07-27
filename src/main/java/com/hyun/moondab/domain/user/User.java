469d02369bfdfb209be1826ad3c75e922910e481
hyunjuuun <hyunjun86120@gmail.com>
2024-07-27 16:26:52 +0900

    User Table명 "users" 사용

--- src/main/java/com/hyun/moondab/domain/user/User.java
+++ src/main/java/com/hyun/moondab/domain/user/User.java
@@ -1,11 +1,9 @@
 package com.hyun.moondab.domain.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
