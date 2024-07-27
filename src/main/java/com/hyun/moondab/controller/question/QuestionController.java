e78b1c36a4c27cf294fd4f7b9c6853f0ae6ce549
hyunjuuun <hyunjun86120@gmail.com>
2024-07-27 16:36:52 +0900

    질문 조회 API 추가

--- src/main/java/com/hyun/moondab/controller/question/QuestionController.java
+++ src/main/java/com/hyun/moondab/controller/question/QuestionController.java
@@ -1,10 +1,13 @@
 package com.hyun.moondab.controller.question;
 
 import com.hyun.moondab.application.question.QuestionService;
-import com.hyun.moondab.controller.question.dto.QuestionDto;
+import com.hyun.moondab.controller.question.dto.QuestionRq;
+import com.hyun.moondab.controller.question.dto.QuestionRs;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,9 +18,10 @@ public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/{id}")
-    public QuestionDto getQuestion(@PathVariable Long id) {
-        return questionService.getQuestion(id);
+    @Operation(summary = "질문 조회", description = "날짜 데이터를 기준으로 질문을 조회한다.")
+    @GetMapping
+    public QuestionRs getQuestion(QuestionRq rq) {
+        return questionService.getQuestion(rq);
     }
 
 }
